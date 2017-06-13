1ba5a301c65b6606d23c83a8dd89fc0b4676d649
Dmitriy <1996@3g.ua>
2017-06-13 13:18:13 +0300

    changing views for groups and redirecting logic

--- resources/views/groups/main.blade.php
+++ resources/views/groups/main.blade.php
@@ -1,18 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container text-center groups-list">
-        <label for="groups-list">Оберіть вашу навчальну групу</label><br>
-        <select name="groups-list" id="groups-list" size="4" required>
-            @foreach($groups as $group)
-                <option>{{$group->title}}</option>
-            @endforeach
-        </select>
-        <br>
-        <button class="btn btn-default">Розклад</button>
 
+    <div class="container text-center groups-list">
+        Оберіть потрібну вам навчальну групу
+        <br>
+        @foreach($groups as $group)
+            <a href="/groups/{{$group->URL_title}}" class="btn btn-default">{{$group->title}}</a>
+        @endforeach
     </div>
 
 
-    <script src="{{asset('/js/groups.js')}}"></script>
 @stop
