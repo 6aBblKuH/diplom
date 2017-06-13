9eadf055e380f8b835d349dd1864c928b3bbba50
Dmitriy <1996@3g.ua>
2017-06-13 16:28:31 +0300

    changes in groups/schedules views

--- resources/views/groups/group_schedule.blade.php
+++ resources/views/groups/group_schedule.blade.php
@@ -2,16 +2,35 @@
 
 @section('content')
     <div class="container">
-        @foreach($schedules as $schedule)
-            <div class="col-md-2 text-center schedule-item">
-                <p>{{$schedule->day_of_week}}</p>
-                <p>{{$schedule->lesson_order}}</p>
-                <p>{{$schedule->lesson_type}}</p>
-                <p>{{$schedule->audience}}</p>
-                <p>{{$schedule->title}}</p>
-                <p>{{$schedule->teacher}}</p>
-            </div>
-        @endforeach
+        <div class="row">
+            <a href="/" class="btn btn-default schedule-back-btn">Назад</a>
+        </div>
+        <table class="schedule-table text-center">
+            <thead>
+            <tr>
+                <td>День тижня</td>
+                <td>Номер пари</td>
+                <td>Тип заняття</td>
+                <td>Аудиторія</td>
+                <td>Дисципліна</td>
+                <td>Викладач</td>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($schedules as $schedule)
+                <tr class="schedule-item">
+                    <td>{{$schedule->day_of_week}}</td>
+                    <td>{{$schedule->lesson_order}}</td>
+                    <td>{{$schedule->lesson_type}}</td>
+                    <td>{{$schedule->audience}}</td>
+                    <td>{{$schedule->title}}</td>
+                    <td>{{$schedule->teacher}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+
+        </table>
+
     </div>
 
 
--- resources/views/groups/main.blade.php
+++ resources/views/groups/main.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container text-center groups-list">
-        Оберіть потрібну вам навчальну групу
+        <div class="text-center text-uppercase"><b>Оберіть потрібну вам навчальну групу</b></div>
         <br>
         @foreach($groups as $group)
             <a href="/groups/{{$group->URL_title}}" class="btn btn-default">{{$group->title}}</a>
